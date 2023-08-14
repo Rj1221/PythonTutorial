27f97d8ae1611531152374c53b30ec93889f7d9d
Raj <rj6207491172@gmail.com>
2023-08-14 14:57:00 +0530

    Example Added for While Loop

--- Loops/while.py
+++ Loops/while.py
@@ -14,13 +14,18 @@ while count <= 5:
 else:
     print("Count is greater than 5")
 
+# Countdown:
 
+# Let's say you want to create a countdown from 5 to 1:
 countdown = 5
 while countdown > 0:
     print(countdown)
     countdown -= 1
 print("Blastoff!")
 
+# User Input:
+
+# Here's an example where the program asks the user for input until they enter "quit":
 
 user_input = ""
 while user_input != "quit":
@@ -28,7 +33,9 @@ while user_input != "quit":
     print("You entered:", user_input)
 
 
+# Sum of Numbers:
 
+# This example calculates the sum of numbers entered by the user until they enter a negative
 total = 0
 num = int(input("Enter a number (enter a negative number to stop): "))
 while num >= 0:
